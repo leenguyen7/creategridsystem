87877e2f815870838e00664c22feb80fa26767d2
Hoang Nguyen <rapclubfamily@gmail.com>
2018-01-22 10:40:19 +0700

    123

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
+var zone = $('#wrapper');
 
-$(document).ready(function() {
-	
-	var dropZone = $('#wrapper');
- 
- 	$('#column1').on("click", function(event){
- 		dropZone.append('<div class="row"><div class="col-12"></div></div>');
- 	})
- 	$('#column2').on("click", function(event){
- 		dropZone.append('<div class="row"><div class="col-6"></div><div class="col-6"></div></div>');
- 	})
- 	$('#column3').on("click", function(event){
- 		dropZone.append('<div class="row"><div class="col-4"></div><div class="col-4"></div><div class="col-4"></div></div>');
- 	})
- 	$('#column4').on("click", function(event){
- 		dropZone.append('<div class="row"><div class="col-3"></div><div class="col-3"></div><div class="col-3"></div><div class="col-3"></div></div>');
- 	})
+var rowVal = {
+    row1: '<div class="col-12"></div>',
+    row2: '<div class="col-6"></div><div class="col-6"></div>',
+    row3: '<div class="col-4"></div><div class="col-4"></div><div class="col-4"></div>',
+    row4: '<div class="col-3"></div><div class="col-3"></div><div class="col-3"></div><div class="col-3"></div>'
+}
 
-});
+var app = {
+    addRow: function (value) {
+        var item = '<div class="row">' + value + '</div>';
+        zone.append(item);
+    },
+
+}
+
+
+
+//	var dropZone = $('#wrapper');
+// 
+// 	$('#column1').on("click", function(event){
+// 		dropZone.append('<div class="row"><div class="col-12"></div></div>');
+// 	})
+// 	$('#column2').on("click", function(event){
+// 		dropZone.append('<div class="row"><div class="col-6"></div><div class="col-6"></div></div>');
